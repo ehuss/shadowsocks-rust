70005dc6a30a88d36032fd795118e953492655e5
Eric Huss <eric@huss.org>
2020-08-19 12:53:54 -0700

    Test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! shadowsocks is a fast tunnel proxy that helps you bypass firewalls.
 //!
-//! Small change8.
+//! Small change9.
 //!
 //! Currently it supports SOCKS5 and HTTP Proxy protocol.
 //!
